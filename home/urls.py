e9455ba07f4c0d3690a6825905565d899bf20059
AbaidooPrince <abaidooprince@gmail.com>
2020-05-20 19:04:19 +0000

    navigation template, footer, modals (login and signup) ready for backend

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+#new s
+
+from django.urls import path
+from .views import TemplateView, HomePageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name = 'home'),
+]
+
+
+
+
+
--- zeeFreeProj/settings.py
+++ zeeFreeProj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig', #new
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'zeeFreeProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+    ]    #new
