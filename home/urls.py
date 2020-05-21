9dbe434b26c0bc155b3bf5c577d3c5c833ba88d8
AbaidooPrince <abaidooprince@gmail.com>
2020-05-21 12:39:37 +0000

    changing modals to pages(less styled)

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 #new s
 
 from django.urls import path
-from .views import TemplateView, HomePageView
+from .views import TemplateView, HomePageView, LoginPageView, SignUpPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name = 'home'),
+    path('login/', LoginPageView.as_view(), name = 'login'),
+    path('register', SignUpPageView.as_view(), name = 'signup'),
 ]
 
 
