ca5b4f1bbbda99021a02cf31a1eeccaaad41c993
Benjamin Andoh <benoo.andoh@gmail.com>
2020-05-21 13:08:48 +0000

    setting up registration and login

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,13 @@
-#new s
+# new s
 
 from django.urls import path
+
+from . import views
 from .views import TemplateView, HomePageView
 
+
 urlpatterns = [
-    path('', HomePageView.as_view(), name = 'home'),
+    path('', HomePageView.as_view(), name='home'),
+    path('register', views.registerPage,   name='register'),
+    path('login', views.loginPage, name='login')
 ]
-
-
-
-
-
--- home/views.py
+++ home/views.py
@@ -3,5 +3,16 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+
+def registerPage(request):
+    context = {}
+    return render(request, 'base/register.html', context)
+
+
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login.html', context)
