8db9435d0b3841b0e1e422b7a0acee42e98d5f3f
Benjamin Andoh <benoo.andoh@gmail.com>
2020-05-21 10:44:56 +0000

    installed virtualenv/django3/mysqlclient/phoneNumberfield

--- zeeFreeProj/settings.py
+++ zeeFreeProj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +80,20 @@ WSGI_APPLICATION = 'zeeFreeProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zeeFreeProj',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS':
+        {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+
     }
 }
 
